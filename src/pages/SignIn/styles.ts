a1e863355c05dffdb6bb0052b5f8b77531d1d5a0
João Nascimento <joao1991algoritmo@gmail.com>
2020-06-17 18:16:21 -0300

    Configuração navegação; densidade pixel;fontes externas

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px; /*Cima; laterais; baixo */
+`;
+
+export { Container, Title };
