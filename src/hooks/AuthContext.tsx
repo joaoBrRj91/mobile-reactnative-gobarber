bb4ac571f95f79669bbdc0b3c275c576af38b89f
João Nascimento <joao1991algoritmo@gmail.com>
2020-06-25 09:51:00 -0300

    Rotas privadas(autenticadas)

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -15,6 +15,7 @@ interface SignInCredentials {
 
 interface AuthContextState {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -28,6 +29,7 @@ const AuthContext = createContext<AuthContextState>({} as AuthContextState);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -42,6 +44,8 @@ const AuthProvider: React.FC = ({ children }) => {
       if (tokenData && userData) {
         setData({ token: tokenData, user: JSON.parse(userData) });
       }
+
+      setLoading(false);
     }
 
     loadStorageData();
@@ -70,7 +74,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
